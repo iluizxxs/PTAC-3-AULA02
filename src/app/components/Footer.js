32535aa539b9bfe0daa14828171bdf91831beaf1
iluizxxs <luiz.silva34@estudante.ifms.edu.br>
2024-03-06 13:38:11 -0400

    0603

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,25 +1,17 @@
-ink from "next/link"
+import Link from "next/link"
 import styles from "./menu.module.css"
 import Image from "next/image";
 
 export default function Menu(){
     return(
-        <header className={styles.cabecalho}>
+        <footer className={styles.cabecalho}>
             <Image
             width={100}
             height={100}
             
-            src={''}/>
+            src={'https://ead.ifms.edu.br/theme/moove/pix/moodle-logo-white.png'}/>
 
-            <nav>
-                <ul>
-                 <Link href="/">
-                    <li>Home</li>
-                </Link>
-                <Link href="/registro">
-                    <li>Registrar</li>
-                </Link>
-                </ul>
-            </nav>
-        </header>
+            
+        </footer>
     );
+}
